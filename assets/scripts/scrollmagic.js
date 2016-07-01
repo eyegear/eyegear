ec9e303f1ffe56506b7c6eb396a8fcfa46c7e9b2
munair <munair@qe.local>
2016-07-02 08:43:35 +0900

    add scrollmagic animations to an .js file

---
+++ assets/scripts/scrollmagic.js
@@ -0,0 +1,30 @@
+/* ========================================================================
+ * DOM-based Scroll Animations
+ * Based on http://scrollmagic.io/examples/basic/simple_tweening.html
+ * ======================================================================== */
+
+// Implement Ribbon Animations when the DOM is ready
+
+$(function() {
+
+	// STEP 1: create / initialize the scrollmagic controller
+	var controller = new ScrollMagic.Controller();
+
+	// STEP 2: create animation object (with a duration of half a second)
+	var tween = TweenMax.to('#brand-animation', 0.5, {
+		letterSpacing: '10px',
+		scale: 1.5,
+		rotation: 360
+	});
+
+	// STEP 3: create scene object (and select scene options)
+		var scene = new ScrollMagic.Scene({
+		triggerElement: "#trigger-steps"
+	})
+
+	// STEP 4: add our animation object to the scene object
+		.setTween(tween)
+		
+	// STEP 5: add our scene object to the scrollmagic controller
+		.addTo(controller);
+)();
