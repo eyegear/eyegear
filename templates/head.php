17a7be0a02ddc38049dcb113c9f0ec52c2cf5941
munair <munair@qe.local>
2016-07-03 08:37:49 +0900

    add banner font

--- templates/head.php
+++ templates/head.php
@@ -1,6 +1,12 @@
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <?php wp_head(); ?>
+	<!--- Head Meta --->
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!--- Google Fonts --->
+	<link href='https://fonts.googleapis.com/css?family=Yanone+Kaffeesatz:400,200,300,700' rel='stylesheet' type='text/css'>
+
+	<!--- Wordpress Head --->
+	<?php wp_head(); ?>
 </head>
