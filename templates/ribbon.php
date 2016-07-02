6678b110890fd643c55d1baa6aa10269c39a1821
munair <munair@qe.local>
2016-07-02 10:58:41 +0900

    remove embedded javascript

--- templates/ribbon.php
+++ templates/ribbon.php
@@ -1,9 +1,5 @@
 <section id="steps" class="ribbon">
 	<div class="container">
-		<script>
-			// STEP 1: create / initialize the scrollmagic controller
-			var controller = new ScrollMagic.Controller();
-		</script>
 		<div id="trigger-steps" class="enumeration"></div>
 		<div id="brand-animation">
     			<a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
@@ -15,21 +11,5 @@
       			endif;
       			?>
     		</nav>
-		<script>
-			// STEP 2: create animation object (with a duration of half a second)
-			var tween = TweenMax.to('#brand-animation', 0.5, {
-				letterSpacing: '10px',
-				scale: 1.5,
-				rotation: 360
-			});
-			// STEP 3: create scene object (and select scene options)
-			var scene = new ScrollMagic.Scene({
-				triggerElement: "#trigger-steps"
-			})
-			// STEP 4: add our animation object to the scene object
-			.setTween(tween) 
-			// STEP 5: add our scene object to the scrollmagic controller
-			.addTo(controller);
-		</script>
   	</div>
 </section>
