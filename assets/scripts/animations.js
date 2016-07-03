8a4de1a4ed4afcd2ac0dd0f6a220a3372f4aeb27
munair <munair@qe.local>
2016-07-03 10:27:53 +0900

    use just one controller and name indicators. tidy up code.

--- assets/scripts/animations.js
+++ assets/scripts/animations.js
@@ -3,9 +3,12 @@
  * Based on http://scrollmagic.io/examples/basic/simple_tweening.html
  * ======================================================================== */
 
-// Implement Header Animations when the DOM is ready
 
 (function() {
+
+	// Implement Header Animations when the DOM is ready
+	// STEP 1 is needed for the first animation, but not
+	// succeeding animations
 
 	// STEP 1: create / initialize the scrollmagic controller
 	var controller = new ScrollMagic.Controller();
@@ -16,45 +19,29 @@
 	});
 
 	// STEP 3: create scene object (and select scene options)
-		var scene = new ScrollMagic.Scene({
+	var scene = new ScrollMagic.Scene({
 		triggerElement: "#trigger-scale",
 		duration: 300 // Setting to 300 makes scrolling like scrubbing the animation over 300 pixels
 	});
 
 	// STEP 4: add our animation object to the scene object
-		scene.setTween(tween);
+	scene.setTween(tween);
 		
 	// STEP 5: add our scene object to the scrollmagic controller
-		scene.addTo(controller);
+	scene.addTo(controller);
 
 	// OPTIONAL: add debug indicators fixed on right side
-		scene.addIndicators();
-})();
+	scene.addIndicators({name: "1 (duration: 300)"});
 
-// Implement Ribbon Animations when the DOM is ready
-
-(function() {
-
-	// STEP 1: create / initialize the scrollmagic controller
-	var controller = new ScrollMagic.Controller();
-
-	// STEP 2: create animation object (with a duration of half a second)
+	// Implement Ribbon Animations when the DOM is ready
 	var tween = TweenMax.to('#brand-animation', 0.5, {
 		letterSpacing: '10px',
 	});
-
-	// STEP 3: create scene object (and select scene options)
-		var scene = new ScrollMagic.Scene({
+	var scene = new ScrollMagic.Scene({
 		triggerElement: "#trigger-steps",
 		duration: 0 // Setting to zero makes the trigger act like a toggle
 	});
-
-	// STEP 4: add our animation object to the scene object
-		scene.setTween(tween);
-		
-	// STEP 5: add our scene object to the scrollmagic controller
-		scene.addTo(controller);
-
-	// OPTIONAL: add debug indicators fixed on right side
-		scene.addIndicators();
+	scene.setTween(tween);
+	scene.addTo(controller);
+	scene.addIndicators({name: "2 (duration: 0)"});
 })();
