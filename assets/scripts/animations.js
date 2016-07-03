0b43c12ef751e3feeb467a3dfe77095d95ac2cf2
munair <munair@qe.local>
2016-07-03 14:00:37 +0900

    add overlay tween

--- assets/scripts/animations.js
+++ assets/scripts/animations.js
@@ -17,7 +17,7 @@
 	// STEP 5: add our scene object to the scrollmagic controller
 	// OPTIONAL: add debug indicators fixed on right side
 
-	// Implement Header Animations when the DOM is ready
+	// Implement Banner Animation when the DOM is ready
 	var controller = new ScrollMagic.Controller();
 	var bannertween = TweenMax.to('#animate-brand', 0.5, {
 		scale: 0.8
@@ -30,6 +30,18 @@
 	bannerscene.addTo(controller);
 	bannerscene.addIndicators({name: "1 (duration: 300)"});
 
+	// Implement Header Overlay Animation when the DOM is ready
+	var overlaytween = TweenMax.to('#animate-overlay', 0.5, {
+		scale: 0.8
+	});
+	var overlayscene = new ScrollMagic.Scene({
+		triggerElement: "#trigger-overlay",
+		duration: 300 // Setting to 300 makes scrolling like scrubbing the animation over 300 pixels
+	});
+	overlayscene.setTween(overlaytween);
+	overlayscene.addTo(controller);
+	overlayscene.addIndicators({name: "2 (duration: 300)"});
+
 	// Implement Ribbon Animations when the DOM is ready
 	var ribbontween = TweenMax.to('#animate-ribbon', 1.5, {
 		letterSpacing: '5px',
@@ -39,5 +51,5 @@
 	});
 	ribbonscene.setTween(ribbontween);
 	ribbonscene.addTo(controller);
-	ribbonscene.addIndicators({name: "2"});
+	ribbonscene.addIndicators({name: "3"});
 })();
