b969ff31e4acaf28dec94ede6034de45448f76f7
munair <munair@qe.local>
2016-07-03 10:39:06 +0900

    make trigger names more logical. make scene names more logical. make animation names more logical.

--- assets/scripts/animations.js
+++ assets/scripts/animations.js
@@ -6,42 +6,39 @@
 
 (function() {
 
-	// Implement Header Animations when the DOM is ready
 	// STEP 1 is needed for the first animation, but not
-	// succeeding animations
+	// succeeding animations (even if you create two 
+	// separate functions.
 
 	// STEP 1: create / initialize the scrollmagic controller
-	var controller = new ScrollMagic.Controller();
-
 	// STEP 2: create animation object (with a duration of half a second)
-	var tween = TweenMax.to('#animate-brand', 0.5, {
+	// STEP 3: create scene object (and select scene options)
+	// STEP 4: add our animation object to the scene object
+	// STEP 5: add our scene object to the scrollmagic controller
+	// OPTIONAL: add debug indicators fixed on right side
+
+	// Implement Header Animations when the DOM is ready
+	var controller = new ScrollMagic.Controller();
+	var bannertween = TweenMax.to('#animate-brand', 0.5, {
 		scale: 1.2
 	});
-
-	// STEP 3: create scene object (and select scene options)
-	var scene = new ScrollMagic.Scene({
+	var bannerscene = new ScrollMagic.Scene({
 		triggerElement: "#trigger-scale",
 		duration: 300 // Setting to 300 makes scrolling like scrubbing the animation over 300 pixels
 	});
-
-	// STEP 4: add our animation object to the scene object
-	scene.setTween(tween);
-		
-	// STEP 5: add our scene object to the scrollmagic controller
-	scene.addTo(controller);
-
-	// OPTIONAL: add debug indicators fixed on right side
-	scene.addIndicators({name: "1 (duration: 300)"});
+	bannerscene.setTween(bannertween);
+	bannerscene.addTo(controller);
+	bannerscene.addIndicators({name: "1 (duration: 300)"});
 
 	// Implement Ribbon Animations when the DOM is ready
-	var tween = TweenMax.to('#brand-animation', 0.5, {
+	var ribbontween = TweenMax.to('#animate-ribbon', 0.5, {
 		letterSpacing: '10px',
 	});
-	var scene = new ScrollMagic.Scene({
-		triggerElement: "#trigger-steps",
+	var ribbonscene = new ScrollMagic.Scene({
+		triggerElement: "#ribbon-trigger",
 		duration: 0 // Setting to zero makes the trigger act like a toggle
 	});
-	scene.setTween(tween);
-	scene.addTo(controller);
-	scene.addIndicators({name: "2 (duration: 0)"});
+	ribbonscene.setTween(ribbontween);
+	ribbonscene.addTo(controller);
+	ribbonscene.addIndicators({name: "2 (duration: 0)"});
 })();
--- templates/ribbon.php
+++ templates/ribbon.php
@@ -1,7 +1,7 @@
 <section id="steps" class="ribbon">
 	<div class="container">
-		<div id="trigger-steps" class="enumeration"></div>
-		<div id="brand-animation">
+		<div id="ribbon-trigger" class="enumeration"></div>
+		<div id="animate-ribbon">
     			<div class="ribbon-text row">
 				<div class="col-sm-2 col-sm-offset-2"><strong>guide: </strong></div>
 				<div class="col-sm-2">step 1</div>
