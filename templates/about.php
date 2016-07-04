dc35743018257a6a15b79fbfe971ffdc6130409d
munair <munair@qe.local>
2016-07-04 19:11:57 +0900

    add about section

---
+++ templates/about.php
@@ -0,0 +1,8 @@
+<section id="section-about" class="about">
+	<div class="container">
+		<div id="trigger-fadeIn" class="fadeIn-animation"></div>
+    		<div id="animate-about-text">
+			<a class="about-text" href="<?= esc_url(home_url('/')); ?>">This section is about my awesome <strong>eyegear</strong> company and introduces our catalog.</a>
+		</div>
+  	</div>
+</section>
