36e942b0ff4cb768e5c4a7855325450950ad0b7e
munair <munair@qe.local>
2016-07-04 14:38:15 +0900

    error message without quotes

--- assets/scripts/animations.js
+++ assets/scripts/animations.js
@@ -32,7 +32,7 @@
 
 	// Implement Header Overlay Animation when the DOM is ready
 	var overlaytween = TweenMax.to('#animate-overlay', 0.5, {
-		rotateY: 180deg
+		rotateY: "180deg"
 	});
 	var overlayscene = new ScrollMagic.Scene({
 		triggerElement: "#trigger-overlay",
