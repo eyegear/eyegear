5c916faea0199896f587903fc7b4fe5843086fc6
munair <munair@qe.local>
2016-07-04 23:48:49 +0900

    where do we stand?

--- templates/about.php
+++ templates/about.php
@@ -1,9 +1,11 @@
 <section id="section-about" class="about">
 	<div class="container">
 		<div id="opacity-trigger" class="opacity-animation"></div>
-    		<div id="animate-about-text">
-			<h1 class="about-text">eyegear catalog</h1>
-			<p class="about-text"><a href="<?= esc_url(home_url('/')); ?>">Eyegear</a> is a wordpress theme designed specially for eyewear companies that want to bring their product catalog to life with eye-catching animation and other HTML5/CSS3/Javascript technologies for mobile devices and desktop computers. This section <strong>section</strong> allows the eyewear company to introduce their unique value proposition and present their eyewear catalog.</p>
+    		<div id="animate-about-text row">
+			<div class="col-sm-10 col-sm-offset-1">
+				<h1 class="about-text">eyegear catalog</h1>
+				<p class="about-text"><a href="<?= esc_url(home_url('/')); ?>">Eyegear</a> is a wordpress theme designed specially for eyewear companies that want to bring their product catalog to life with eye-catching animation and other HTML5/CSS3/Javascript technologies for mobile devices and desktop computers. This section <strong>section</strong> allows the eyewear company to introduce their unique value proposition and present their eyewear catalog.</p>
+			</div>
 		</div>
   	</div>
 </section>
