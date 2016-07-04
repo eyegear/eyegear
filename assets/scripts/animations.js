89c7f81c1da64a7c58fd92901ab9e54e16de8502
munair <munair@qe.local>
2016-07-04 14:35:32 +0900

    take out quotes

--- assets/scripts/animations.js
+++ assets/scripts/animations.js
@@ -32,7 +32,7 @@
 
 	// Implement Header Overlay Animation when the DOM is ready
 	var overlaytween = TweenMax.to('#animate-overlay', 0.5, {
-		scale: 1.8
+		rotateY: 180deg
 	});
 	var overlayscene = new ScrollMagic.Scene({
 		triggerElement: "#trigger-overlay",
