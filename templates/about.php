95c206276285b54b36372362cd2460055364aec7
munair <munair@qe.local>
2016-07-06 05:42:42 +0900

    my html was all messed up

--- templates/about.php
+++ templates/about.php
@@ -1,8 +1,8 @@
 <section id="section-about" class="about">
 	<div class="container">
-		<div id="trigger-opacity" class="opacity-animation"></div>
-    		<div id="animate-about-text row">
-			<div class="about-text col-sm-4 col-sm-offset-4">
+    		<div class="row">
+			<div id="trigger-opacity" class="opacity-animation"></div>
+			<div id="animate-about-text" class="about-text col-sm-4 col-sm-offset-4">
 				<h1>eyegear catalog</h1>
 				<p><a href="<?= esc_url(home_url('/')); ?>">Eyegear</a> is a wordpress theme designed specially for eyewear companies that want to bring their product catalog to life with eye-catching animation and other HTML5/CSS3/Javascript technologies for mobile devices and desktop computers. This section <strong>section</strong> allows the eyewear company to introduce their unique value proposition and present their eyewear catalog.</p>
 			</div>
