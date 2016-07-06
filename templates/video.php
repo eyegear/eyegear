ca333acc00960b3ada536631c358cb6db3bbad5a
munair <munair@qe.local>
2016-07-06 22:53:23 +0900

    remove iframe

--- templates/video.php
+++ templates/video.php
@@ -2,7 +2,6 @@
 	<div class="container">
     		<div class="row">
 			<div id="video-underlay" class="underlay"></div>
-			<iframe src="https://youtu.be/C7IJ7npTYrU" width="100%"></ifram>
 		</div>
   	</div>
 </section>
