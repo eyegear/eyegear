47d04370a515c4d47c64bc7cd3cab94effcc7d70
munair <munair@qe.local>
2016-07-06 16:41:23 +0900

    take out possibly buggy sections

--- templates/catalog.php
+++ templates/catalog.php
@@ -5,7 +5,7 @@
 					<h1>sun man</h1>
 			</div>
 			<div class="col-md-6">
-				<div id="trigger-featured-image" class="slide-featured-image"></div>
+				<div id="trigger-featured-image" class="featured-image"></div>
     				<div id="animate-featured-image">
 					<img id="featured-image" class="catalog-image" src="https://s3.ap-northeast-2.amazonaws.com/eyegear/oakley-style-1500x588px.jpg">
 				</div>
