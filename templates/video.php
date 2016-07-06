d8c98a13391c0b18dfd09ac8d29d954689319b28
munair <munair@qe.local>
2016-07-06 23:08:37 +0900

    fullscreen video implementation

--- templates/video.php
+++ templates/video.php
@@ -1,7 +1,9 @@
 <section id="section-video" class="video">
 	<div class="container">
     		<div class="row">
-			<div id="video-underlay" class="underlay"></div>
+			<div id="video-underlay" class="underlay">
+				<iframe id="video" src="//www.youtube.com/embed/C7IJ7npTYrU?rel=0&autoplay=1" frameborder="0" allowfullscreen></iframe>
+			</div>
 		</div>
   	</div>
 </section>
