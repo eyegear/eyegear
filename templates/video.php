e7982b66d443cf225691011e5c2a47b375cee461
munair <munair@qe.local>
2016-07-06 16:21:55 +0900

    remove embedded video from underlayer

--- templates/video.php
+++ templates/video.php
@@ -1,9 +1,8 @@
 <section id="section-video" class="video">
 	<div class="container">
     		<div class="row">
-			<div id="video-underlay" class="underlay">
-				<iframe src="https://youtu.be/C7IJ7npTYrU" width="100%"></ifram>
-			</div>
+			<div id="video-underlay" class="underlay"></div>
+			<iframe src="https://youtu.be/C7IJ7npTYrU" width="100%"></ifram>
 		</div>
   	</div>
 </section>
