8c1846e35cb92bc235beaac07d78a26337b932da
munair <munair@qe.local>
2016-07-06 16:03:12 +0900

    fix appearance of about section for mobile

--- templates/about.php
+++ templates/about.php
@@ -2,7 +2,7 @@
 	<div class="container">
     		<div class="row">
 			<div id="trigger-opacity" class="opacity-animation"></div>
-			<div id="animate-about-text" class="about-text col-sm-4 col-sm-offset-4">
+			<div id="animate-about-text" class="about-text col-xs-10 col-xs-offset-1 col-sm-4 col-sm-offset-4">
 				<h1>eyegear catalog</h1>
 				<p><a href="<?= esc_url(home_url('/')); ?>">Eyegear</a> is a wordpress theme designed specially for eyewear companies that want to bring their product catalog to life with eye-catching animation and other HTML5/CSS3/Javascript technologies for mobile devices and desktop computers. This section <strong>section</strong> allows the eyewear company to introduce their unique value proposition and present their eyewear catalog.</p>
 			</div>
