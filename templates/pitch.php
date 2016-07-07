4c483550894fdc30242860feb9767e8b16450de5
munair <munair@qe.local>
2016-07-07 16:00:48 +0900

    add padding

--- templates/pitch.php
+++ templates/pitch.php
@@ -1,9 +1,10 @@
-<section id="marketing" class="pitch">
+<section id="section-pitch" class="pitch">
 	<div class="container">
 		<div id="pitch-trigger" class="pitch-animation"></div>
 		<div id="animate-pitch">
-    			<div class="pitch-text row">
+    			<div class="pitch-text row padded">
 				<div class="col-sm-2 col-sm-offset-5">order <strong>now</strong></div>
+			</div>
 		</div>
   	</div>
 </section>
