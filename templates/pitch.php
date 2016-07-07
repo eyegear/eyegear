bc53ca45121f204d38473221419a0560fe356962
munair <munair@qe.local>
2016-07-07 19:56:35 +0900

    apply button styling to column not row

--- templates/pitch.php
+++ templates/pitch.php
@@ -2,8 +2,8 @@
 	<div class="container">
 		<div id="pitch-trigger" class="pitch-animation"></div>
 		<div id="animate-pitch">
-    			<div class="pitch-text row padded">
-				<div class="col-sm-2 col-sm-offset-5">order <strong>now</strong></div>
+    			<div class="row">
+				<div class="pitch-text col-sm-2 col-sm-offset-5">order <strong>now</strong></div>
 			</div>
 		</div>
   	</div>
