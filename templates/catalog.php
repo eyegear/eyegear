aa11820ea42d6a1bb21a5fbf75ba55e6d789c9c1
munair <munair@qe.local>
2016-07-08 15:58:04 +0900

    one section too many?

--- templates/catalog.php
+++ templates/catalog.php
@@ -33,16 +33,5 @@
 						</div>
 					</div>
 				</div>
-				<div class="panel row">
-					<div class="col-md-6">
-							<h1>optical woman</h1>
-					</div>
-					<div class="col-md-6">
-						<div id="trigger-twirl" class="twirling-image"></div>
-		    				<div id="animate-featured-image">
-							<img id="featured-image" class="catalog-image" src="https://s3.ap-northeast-2.amazonaws.com/eyegear/oakley-style-1500x588px.jpg">
-						</div>
-					</div>
-				</div>
   	</div>
 </section>
