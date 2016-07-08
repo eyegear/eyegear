f73d7519d9f0c1766344ce7e8d9cd09594d16c68
munair <munair@qe.local>
2016-07-08 15:43:08 +0900

    bust out row in a separate div

--- templates/catalog.php
+++ templates/catalog.php
@@ -1,6 +1,6 @@
 <section id="section-catalog" class="catalog">
 	<div id="showcase-container" class="container">
-				<div class="panel row">
+				<div class="panel"><div class="row">
 					<div class="col-md-6">
 							<h1>sun man</h1>
 					</div>
@@ -43,6 +43,7 @@
 							<img id="featured-image" class="catalog-image" src="https://s3.ap-northeast-2.amazonaws.com/eyegear/oakley-style-1500x588px.jpg">
 						</div>
 					</div>
+				</div>
 				</div>
   	</div>
 </section>
