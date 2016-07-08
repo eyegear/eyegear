9e49d6666f88cca61c66aa051d7e278e2cdba64c
munair <munair@qe.local>
2016-07-08 10:03:00 +0900

    fix pitch button for iphone

--- templates/pitch.php
+++ templates/pitch.php
@@ -3,7 +3,9 @@
 		<div id="pitch-trigger" class="pitch-animation"></div>
 		<div id="animate-pitch">
     			<div class="row">
-				<div class="pitch-text col-sm-2 col-sm-offset-5">shop <strong>now</strong></div>
+				<div class="col-sm-2 col-sm-offset-5">
+					<div class="pitch-text"> shop <strong>now</strong></div>
+				</div>
 			</div>
 		</div>
   	</div>
