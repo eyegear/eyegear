4f327dcc6e0e8795017353e9df49c5989e2d49db
munair <munair@qe.local>
2016-07-08 15:04:18 +0900

    implement slides

--- templates/catalog.php
+++ templates/catalog.php
@@ -1,15 +1,52 @@
 <section id="section-catalog" class="catalog">
 	<div class="container">
-		<div class="row">
-			<div class="col-md-6">
-					<h1>sun man</h1>
-			</div>
-			<div class="col-md-6 viewport-centered">
-				<div id="trigger-twirl" class="twirling-image"></div>
-    				<div id="animate-featured-image">
-					<img id="featured-image" class="catalog-image" src="https://s3.ap-northeast-2.amazonaws.com/eyegear/oakley-style-1500x588px.jpg">
+		<div id="pinContainer" class="pin">
+			<div id="slideContainer" class="slide">
+				<div class="panel row">
+					<div class="col-md-6">
+							<h1>sun man</h1>
+					</div>
+					<div class="col-md-6 viewport-centered">
+						<div id="trigger-twirl" class="twirling-image"></div>
+		    				<div id="animate-featured-image">
+							<img id="featured-image" class="catalog-image" src="https://s3.ap-northeast-2.amazonaws.com/eyegear/oakley-style-1500x588px.jpg">
+						</div>
+					</div>
 				</div>
-			</div>
-		</div>
+				<div class="panel row">
+					<div class="col-md-6">
+							<h1>sun woman</h1>
+					</div>
+					<div class="col-md-6 viewport-centered">
+						<div id="trigger-twirl" class="twirling-image"></div>
+		    				<div id="animate-featured-image">
+							<img id="featured-image" class="catalog-image" src="https://s3.ap-northeast-2.amazonaws.com/eyegear/oakley-style-1500x588px.jpg">
+						</div>
+					</div>
+				</div>
+				<div class="panel row">
+					<div class="col-md-6">
+							<h1>optical man</h1>
+					</div>
+					<div class="col-md-6 viewport-centered">
+						<div id="trigger-twirl" class="twirling-image"></div>
+		    				<div id="animate-featured-image">
+							<img id="featured-image" class="catalog-image" src="https://s3.ap-northeast-2.amazonaws.com/eyegear/oakley-style-1500x588px.jpg">
+						</div>
+					</div>
+				</div>
+				<div class="panel row">
+					<div class="col-md-6">
+							<h1>optical woman</h1>
+					</div>
+					<div class="col-md-6 viewport-centered">
+						<div id="trigger-twirl" class="twirling-image"></div>
+		    				<div id="animate-featured-image">
+							<img id="featured-image" class="catalog-image" src="https://s3.ap-northeast-2.amazonaws.com/eyegear/oakley-style-1500x588px.jpg">
+						</div>
+					</div>
+				</div>
+  			</div>
+  		</div>
   	</div>
 </section>
